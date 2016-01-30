7fc3b02ebf9753aed52dcacb549dbd22bb5f6d96
krisoll <benitez.diego2@gmail.com>
2016-01-30 20:07:34 -0300

    Mariposa

--- Assets/Scripts/Grimbo.cs
+++ Assets/Scripts/Grimbo.cs
@@ -11,7 +11,7 @@ public class Grimbo : MonoBehaviour {
     private RaycastHit2D enSuelo;
     private BoxCollider2D box;
     private bool flipped;
-    private bool butterflied;
+    private bool empowered;
     private bool drawing;
     private Animator anim;
     private GameObject drawingInstance;
@@ -21,6 +21,8 @@ public class Grimbo : MonoBehaviour {
     private float prevVelY;
     public bool activated = true;
     public bool switched = false;
+    private bool action;
+    Drawing.DrawingType draw;
 	// Use this for initialization
 	void Start () {
         rigid = GetComponent<Rigidbody2D>();
@@ -35,31 +37,53 @@ public class Grimbo : MonoBehaviour {
         anim.SetFloat("Velocity", Mathf.Abs(rigid.velocity.x));
         anim.SetFloat("VelocidadCaida", rigid.velocity.y);
         if (!activated) return;
-        if (!drawing && !snapped) rigid.velocity = new Vector2(Input.GetAxis("Horizontal") * velocity, rigid.velocity.y);
-        else if (snapped) rigid.velocity = new Vector2(0, rigid.velocity.y);
+        if (draw == Drawing.DrawingType.NONE)
+        {
+            if (!drawing && !snapped) rigid.velocity = new Vector2(Input.GetAxis("Horizontal") * velocity, rigid.velocity.y);
+            else if (snapped) rigid.velocity = new Vector2(0, rigid.velocity.y);
+            if (enSuelo)
+            {
+                if (Input.GetButtonDown("Fire3") && !drawing)
+                {
+                    if (drawingInstance != null)
+                    {
+                        Destroy(drawingInstance);
+                    }
+                    drawing = true;
+                    anim.SetBool("Drawing", true);
+                }
+                if (Input.GetButtonDown("Fire1") && drawingInstance != null)
+                {
+                    setActive(false);
+                    drawingScr.setActive(true);
+                    Manager.gManager.asignedPlayer = drawingInstance;
+                    switched = true;
+                }
+            }
+        }
+        else if (draw == Drawing.DrawingType.BUTTERFLY)
+        {
+            if (!drawing && !snapped && !action) rigid.velocity = new Vector2(Input.GetAxis("Horizontal") * velocity, rigid.velocity.y);
+            else if (snapped && !action) rigid.velocity = new Vector2(0, rigid.velocity.y);
+            else if (action&&rigid.velocity.y<=-1.2f) rigid.velocity = new Vector2(Input.GetAxis("Horizontal") * velocity, -1.2f);
+            if (Input.GetButtonDown("Fire3"))
+            {
+                action = true;
+                anim.SetBool("Action", action);
+            }
+            if (Input.GetButtonUp("Fire3"))
+            {
+                action = false;
+                anim.SetBool("Action", action);
+            }
+        }
         enSuelo = Physics2D.BoxCast(transform.position+new Vector3(box.offset.x,box.offset.y,0), new Vector2(box.size.x, box.size.x), 0, new Vector2(0, -1), (box.size.y/2)+0.01f, LayerMask.GetMask("Map"));
         if (enSuelo)
         {
             if (!anim.GetBool("EnSuelo")) anim.SetBool("EnSuelo", true);
-            if (Input.GetAxis("Vertical")>0.01f)
+            if (Input.GetAxis("Vertical")>0.01f&&!drawing)
             {
                 rigid.velocity = new Vector2(rigid.velocity.x, jumpVel);
-            }
-            if (Input.GetButtonDown("Fire3")&&!drawing)
-            {
-                if (drawingInstance != null) 
-                {
-                    Destroy(drawingInstance);
-                }
-                drawing = true;
-                anim.SetBool("Drawing", true);
-            }
-            if (Input.GetButtonDown("Fire1")&&drawingInstance!=null)
-            {
-                setActive(false);
-                drawingScr.setActive(true);
-                Manager.gManager.asignedPlayer = drawingInstance;
-                switched = true;
             }
         }
         else
@@ -131,5 +155,14 @@ public class Grimbo : MonoBehaviour {
     {
         activated = a;
         if(!a)rigid.velocity = new Vector2(0, 0);
+    }
+    public void empower(int a)
+    {
+        if (a == 1)
+        {
+            draw = Drawing.DrawingType.BUTTERFLY;
+            empowered = true;
+        }
+        anim.SetInteger("Power", a);
     }
 }
