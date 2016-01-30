390de6b86e54109fc5167d00681a8d2c84f0847c
Juan Valdes <valdes.juan@outlook.es>
2016-01-30 04:04:13 -0300

    Manager

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+    public static Manager gManager;
+    public Grimbo player;
+
+	// Use this for initialization
+	void Start () {
+        gManager = new Manager();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
