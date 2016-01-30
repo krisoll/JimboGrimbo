5e6765fcb6171397280afcbd1c7aed7534b16779
krisoll <benitez.diego2@gmail.com>
2016-01-30 09:33:44 -0300

    Tiles

---
+++ Assets/Tilesets/Tile Basico.tsx
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tile Basico" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="Titles suelo.png" width="96" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="0" x="139.5" y="38.25"/>
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
