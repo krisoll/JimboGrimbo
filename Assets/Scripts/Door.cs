4c6f99afd3fc346e3658757b8c4f9e1bd5afd774
krisoll <benitez.diego2@gmail.com>
2016-01-30 22:20:14 -0300

    AAAAAAAAAAAA

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+    public int nextLevel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            if (Input.GetAxis("Vertical") > 0.01)
+            {
+                Application.LoadLevel(nextLevel);
+            }
+        }
+    }
+}
