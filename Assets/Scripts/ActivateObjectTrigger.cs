f5812391c0ca90d69d12cb4e3c99ec04e10631ad
krisoll <benitez.diego2@gmail.com>
2016-01-31 12:59:42 -0300

    Activate Object Trigger

---
+++ Assets/Scripts/ActivateObjectTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateObjectTrigger : MonoBehaviour {
+    public GameObject[] activate;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            for (int i = 0; i < activate.Length; i++)
+            {
+                activate[i].SetActive(true);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
